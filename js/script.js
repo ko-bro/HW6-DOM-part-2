ce925086b031a654c001d90d06643127e6a60284
Mykola Bronovytskyi <bronovytskyi.m@gmail.com>
2021-07-18 00:10:42 +0300

    figure and black color fix

--- js/script.js
+++ js/script.js
@@ -2,44 +2,18 @@ let figure = document.querySelector('.figure');
 
 let shape = document.getElementById('figSelect').value;
 
-
-switch (shape) {
-    case 'square':
-        figure.style.cssText = 'width: 100px; height: 100px';
-        break;
-
-    case 'rectangle':
-        figure.style.width = '200px';
-        break;
-    
-    case 'circle': 
-        figure.style.cssText = 'width: 100px; height: 100px; border-radius: 50%';
-        break;
-}
-
-
+figSelect.addEventListener('change', function (selection) {
+    figure.className = selection.target.value;
+});
 
 let btn = document.querySelector('button');
 
-btn.onclick = function colorPick() {
+btn.addEventListener('click', function colorPick() {
     let shapeColor = document.querySelector('input').value;
-
-    figure.style.backgroundColor = shapeColor;
-}
-
-
-
-// if (shapeColor = ' ') {
-//     figure.style.backgroundColor = 'black';
-// }
-
-
-// if (shape === 'square') {
-//     figure.style.cssText='width: 100px; height: 100px';
-
-// } else if (shape === 'rectangle') {
-//     figure.style.cssText='width: 200px; height: 200px';
-
-// } else {
-//     figure.style.borderRadius='50%';
-// }
+    
+    if (shapeColor.length == 0) {
+        figure.style.backgroundColor = 'black';
+    } else {
+        figure.style.backgroundColor = shapeColor;
+    }
+});
